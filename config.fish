33fcb24b3af780cbc42b0684127f2501dd57712c
Clemens Wältken <clemens@waltken.de>
2021-07-29 15:26:13 +0200

    Move plugins into interactive mode settings

--- config.fish
+++ config.fish
@@ -2,6 +2,11 @@ if status is-interactive
   # Commands to run in interactive sessions can go here
   set -gx GPG_TTY (tty)
 	if not functions -q fundle; eval (curl -sfL https://git.io/fundle-install); end
+
+	fundle plugin 'oh-my-fish/plugin-thefuck'
+	fundle plugin 'danhper/fish-ssh-agent'
+
+	fundle init
 end
 
 if status is-login
@@ -10,10 +15,5 @@ if status is-login
   fish_add_path ~/.bin/
   fish_add_path ~/.yarn/bin/
 end
-
-fundle plugin 'oh-my-fish/plugin-thefuck'
-fundle plugin 'danhper/fish-ssh-agent'
-
-fundle init
 
 set fish_greeting
