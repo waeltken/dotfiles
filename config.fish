fe0f03d5c24d4f310664a553b95ed8d0208050be
Clemens Wältken <clemens@waltken.de>
2021-07-29 15:06:59 +0200

    Update aliases

--- config.fish
+++ config.fish
@@ -1,12 +1,13 @@
 if status is-interactive
   # Commands to run in interactive sessions can go here
+  set -gx GPG_TTY (tty)
 end
 
 if status is-login
   # Commands to run in login shells can go here
+	source ~/.shell/aliases.sh
   fish_add_path ~/.bin/
   fish_add_path ~/.yarn/bin/
-  set -gx GPG_TTY (tty)
 end
 
 set fish_greeting
--- shell/aliases.sh
+++ shell/aliases.sh
@@ -2,6 +2,7 @@
 alias g=git
 alias dc=docker-compose
 alias go=richgo
+alias gst="git status"
 #alias vi=vim
 #alias vim=vim
 #alias man="man -Hfirefox"
