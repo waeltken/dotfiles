709658b07d6f79df4071a3ecbba14635a91b0a8c
Clemens Wältken <clemens.waltken@microsoft.com>
2023-06-28 10:16:41 +0200

    Move xdg config folders into subfolder

---
+++ config/fish/config.fish
@@ -0,0 +1,40 @@
+if status is-interactive
+  # Commands to run in interactive sessions can go here
+  set -gx GPG_TTY (tty)
+  set -gx FUNCTIONS_CORE_TOOLS_TELEMETRY_OPTOUT 0
+
+  if not functions -q fundle; eval (curl -sfL https://git.io/fundle-install); end
+
+  set -U PROJECT_PATHS ~/workspace
+  set -gx DISPLAY :0
+
+  eval (/home/linuxbrew/.linuxbrew/bin/brew shellenv)
+
+  # Shell plugins and completions
+  navi widget fish | source
+  direnv hook fish | source
+  starship init fish | source
+  kubectl completion fish | source
+
+  # Add aliases
+  source ~/.shell/aliases.sh
+	# Add paths
+  fish_add_path ~/.bin/
+  fish_add_path ~/.yarn/bin/
+  fish_add_path ~/.krew/bin
+end
+
+if status is-login
+  # Commands to run in login shells can go here
+end
+
+set fish_greeting
+
+
+set -gx FUNCTIONS_CORE_TOOLS_TELEMETRY_OPTOUT 1
+set -gx PATH "/home/clwaltke/.local/bin" $PATH
+
+# pnpm
+set -gx PNPM_HOME "/home/clwaltke/.local/share/pnpm"
+set -gx PATH "$PNPM_HOME" $PATH
+# pnpm end
