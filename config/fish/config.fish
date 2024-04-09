34730122d633938c187164cea9a866faff62c950
Clemens Wältken <clemens@waeltken.de>
2024-04-09 10:45:37 +0200

    Update PATH settings
    Signed-off-by: Clemens Wältken <clemens@waeltken.de>

--- config/fish/config.fish
+++ config/fish/config.fish
@@ -27,8 +27,9 @@ if status is-interactive
   source ~/.shell/aliases.sh
 	# Add paths
   fish_add_path ~/.bin/
+  fish_add_path ~/.local/bin/
   fish_add_path ~/.yarn/bin/
-  fish_add_path ~/.krew/bin
+  fish_add_path ~/.jbang/bin/
 end
 
 if status is-login
