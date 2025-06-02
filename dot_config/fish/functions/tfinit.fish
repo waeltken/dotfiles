cfb343d49ce5ae0934f5cac094ca715474cbb176
Clemens Wältken <clemens.waltken@microsoft.com>
2025-06-02 21:55:18 +0200

    Add tfinit function

---
+++ dot_config/fish/functions/tfinit.fish
@@ -0,0 +1,9 @@
+function tfinit
+  set key (basename (pwd))".tfstate"
+  terraform init \
+    -backend-config="resource_group_name=default" \
+    -backend-config="storage_account_name=tfstatecwcontoso" \
+    -backend-config="container_name=tfstate" \
+    -backend-config="key=$key" \
+    $argv
+  end
