949d163ec17c262fe923219044495dc568091c6d
Clemens Wältken <clemens@waeltken.de>
2025-06-26 17:59:32 +0200

    Open *.fish.tmpl with syntax highlighting

--- dot_config/nvim/lua/options.lua
+++ dot_config/nvim/lua/options.lua
@@ -1,5 +1,11 @@
 require "nvchad.options"
 
+vim.api.nvim_create_autocmd({"BufRead","BufNewFile"}, {
+  pattern = "*.fish.tmpl",
+  callback = function()
+    vim.bo.filetype = "fish"
+  end,
+})
 -- add yours here!
 
 -- local o = vim.o
