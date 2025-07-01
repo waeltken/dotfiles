7d4b1ce0a81d720ff7c78910917a4841b2c6b014
Clemens Wältken <clemens.waltken@microsoft.com>
2025-07-01 12:16:41 +0200

    Swap dependent plugin for CopilotChat

--- dot_config/nvim/lua/plugins/init.lua
+++ dot_config/nvim/lua/plugins/init.lua
@@ -26,7 +26,8 @@ return {
     "CopilotC-Nvim/CopilotChat.nvim",
     lazy = false,
     dependencies = {
-      { "github/copilot.vim" }, -- or zbirenbaum/copilot.lua
+      { "zbirenbaum/copilot.lua" }, -- or github/copilot.vim
+      -- { "github/copilot.vim" }, -- or zbirenbaum/copilot.lua
       { "nvim-lua/plenary.nvim", branch = "master" }, -- for curl, log and async functions
     },
     build = "make tiktoken", -- Only on MacOS or Linux
