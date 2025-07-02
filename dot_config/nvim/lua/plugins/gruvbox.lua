906833ff0ce1ea2907a7b0ddeb176f28fa7a9cde
Clemens Wältken <clemens.waltken@microsoft.com>
2025-07-02 16:48:25 +0200

    Add gruvbox and srcery colorschemes as plugins

---
+++ dot_config/nvim/lua/plugins/gruvbox.lua
@@ -0,0 +1,18 @@
+return {
+  {
+    "morhetz/gruvbox.nvim",
+    name = "gruvbox",
+    lazy = true,
+    config = function()
+      vim.o.background = "dark"
+      vim.g.gruvbox_contrast_dark = "hard"
+      vim.g.gruvbox_invert_selection = "0"
+      vim.g.gruvbox_italic = true
+      vim.g.gruvbox_italic_comments = true
+      vim.g.gruvbox_italic_keywords = true
+      vim.g.gruvbox_italic_functions = true
+      vim.g.gruvbox_italic_variables = true
+      vim.cmd.colorscheme("gruvbox")
+    end,
+  },
+}
---
+++ dot_config/nvim/lua/plugins/srcery.lua
@@ -0,0 +1,20 @@
+-- add srcery theme to the list of themes
+return {
+  {
+    "srcery-colors/srcery-vim",
+    lazy = true,
+    name = "srcery",
+    config = function()
+      vim.o.background = "dark"
+      vim.g.srcery_italic_comments = true
+      vim.g.srcery_italic_keywords = true
+      vim.g.srcery_italic_functions = true
+      vim.g.srcery_italic_variables = true
+      vim.g.srcery_italic_strings = true
+      vim.g.srcery_italic_numbers = true
+      vim.g.srcery_italic_preproc = true
+      vim.g.srcery_italic_types = true
+      vim.cmd.colorscheme("srcery")
+    end,
+  },
+}
