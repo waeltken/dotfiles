ed2859934bb2e4e84b9dba95a7fd4fe4d16db087
Clemens Wältken <clemens.waltken@microsoft.com>
2025-07-02 16:47:58 +0200

    Disable auto-save

--- dot_config/nvim/lua/plugins/auto-save.lua
+++ dot_config/nvim/lua/plugins/auto-save.lua
@@ -1,8 +1,9 @@
 return {
   {
     "Pocco81/auto-save.nvim",
+    enabled = false,
     config = function()
-      require("auto-save").setup {}
+      require("auto-save").setup({})
     end,
-  }
+  },
 }
