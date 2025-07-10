c754e20f0ff3a53dd5bede2a800c1ac9ccc6f695
Clemens Wältken <clemens.waltken@microsoft.com>
2025-07-10 16:01:11 +0200

    Improve tfinit function

--- dot_config/fish/functions/tfinit.fish
+++ dot_config/fish/functions/tfinit.fish
@@ -1,10 +1,17 @@
 function tfinit
-  set key (basename (pwd))".tfstate"
-  terraform init \
-    -backend-config="resource_group_name=default" \
-    -backend-config="storage_account_name=tfstatecwcontoso" \
-    -backend-config="container_name=tfstate" \
-    -backend-config="key=$key" \
-    -backend-config="use_azuread_auth=true" \
-    $argv
-  end
+    set key
+    set args $argv
+    if test (count $argv) -gt 0
+        set key $argv[1]
+        set args $argv[2..-1]
+    else
+        set key (basename (pwd))".tfstate"
+    end
+    terraform init \
+        -backend-config="resource_group_name=default" \
+        -backend-config="storage_account_name=tfstatecwcontoso" \
+        -backend-config="container_name=tfstate" \
+        -backend-config="key=$key" \
+        -backend-config="use_azuread_auth=true" \
+        $args
+end
