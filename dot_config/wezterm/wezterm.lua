0b7daa87304c10aeabb454e753012c051bc2c444
Clemens Wältken <clemens@waeltken.de>
2025-07-17 14:38:54 +0200

    Create wezterm config for windows

---
+++ dot_config/wezterm/wezterm.lua
@@ -0,0 +1,31 @@
+-- Pull in the wezterm API
+local wezterm = require 'wezterm'
+
+-- This will hold the configuration.
+local config = wezterm.config_builder()
+
+-- This is where you actually apply your config choices.
+config.wsl_domains = {
+  {
+    -- The name of this specific domain.  Must be unique amonst all types
+    -- of domain in the configuration file.
+    name = 'WSL:Ubuntu',
+
+    -- The name of the distribution.  This identifies the WSL distribution.
+    -- It must match a valid distribution from your `wsl -l -v` output in
+    -- order for the domain to be useful.
+    distribution = 'Ubuntu',
+  },
+}
+config.default_domain = 'WSL:Ubuntu'
+
+-- For example, changing the initial geometry for new windows:
+config.initial_cols = 120
+config.initial_rows = 28
+
+-- or, changing the font size and color scheme.
+config.font_size = 10
+config.color_scheme = 'AdventureTime'
+
+-- Finally, return the configuration to wezterm:
+return config
