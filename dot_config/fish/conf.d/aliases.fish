d28d11f4b72368f3dff1fade864455696fb46512
Clemens Wältken <clemens.waltken@microsoft.com>
2025-10-07 15:08:36 +0200

    Update aliases file that was not correct for fish

---
+++ dot_config/fish/conf.d/aliases.fish
@@ -0,0 +1,14 @@
+# Fish shell aliases
+alias g git
+alias glg "git log"
+alias dc docker-compose
+alias go richgo
+alias gst "git status"
+alias v nvim
+alias c code
+alias cm chezmoi
+alias cz chezmoi
+alias ce "chezmoi edit --watch"
+alias kc kubectl
+alias tf terraform
+alias rzi "rm -rf **/*Zone.Identifier"
